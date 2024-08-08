2d46c366e6b502f7d4478dd9168ac0c39e72bf87
masudur0823 <masudur0823@gmail.com>
2024-08-08 19:41:47 +0600

    edited

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,6 @@
-const { username, password } = process.env;
+// const { username, password } = process.env;
 
 // console.log("username:===:", username);
 // console.log("password:===:", password);
 
-export const connectionString = `mongodb+srv://masud:${password}@cluster0.5dy0m.mongodb.net/taskDB?retryWrites=true&w=majority&appName=Cluster0`;
+export const connectionString = `mongodb+srv://masud:MaSuDmArJaN@cluster0.5dy0m.mongodb.net/taskDB?retryWrites=true&w=majority&appName=Cluster0`;
